eb10eaa500e300ff5c6a5e521c2209c3ed0fdc84
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2023-04-15 00:26:04 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 // const dotenv = require('dotenv');
 
 const connection = mysql.createConnection({
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12612708',
-    password: 'Fadt5rWQW9',
-    database: 'sql12612708',
+    host: 'Your hoster server',
+    user: 'your user',
+    password: 'your password',
+    database: 'your database',
 });
 
 if(connection.state === 'disconnected') {
